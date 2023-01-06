57a58160c965a688c2a40daaf71b275dbedac1af
Okuzawa <kerotyann7@gmail.com>
2023-01-07 05:00:13 +0900

    お試しビルド

---
+++ docs/js/about.6ea70548.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunktest_project"]=self["webpackChunktest_project"]||[]).push([[443],{7381:function(t,e,n){n.r(e),n.d(e,{default:function(){return l}});var r=n(6252);const s={class:"about"},u=(0,r._)("h1",null,"This is an about page",-1),a=[u];function c(t,e){return(0,r.wg)(),(0,r.iD)("div",s,a)}var o=n(3744);const i={},f=(0,o.Z)(i,[["render",c]]);var l=f}}]);
+//# sourceMappingURL=about.6ea70548.js.map
