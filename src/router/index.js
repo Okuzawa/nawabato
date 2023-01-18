7bb8d51a06e964c2212c449b98441fa98d0d243f
Okuzawa <kerotyann7@gmail.com>
2023-01-19 00:23:52 +0900

    github.io用にビルド

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
   {
-    path: '/',
+    path: '/nawabato',
     name: 'title',
     //component: HomeView
     component: () => import(/* webpackChunkName: "title" */ '../views/TitleView.vue')
